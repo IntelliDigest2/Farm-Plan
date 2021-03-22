7df1e6e46ab15947f25ab28be3fe44d74bccd706
iTrackerPlatform <80331423+iTrackerPlatform@users.noreply.github.com>
2021-03-22 10:51:17 +0000

    adding a map to the website

--- src/pages/account/MapData.js
+++ src/pages/account/MapData.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "../Pages.css"
 import { Row, Col} from "react-bootstrap";
-
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function Map() {
 
@@ -15,11 +14,17 @@ function Map() {
 
         <Col className="" xs={12} lg={1}></Col>
         <Col className="" xs={12} lg={10}>
-          <div className="map-data justify-content-center align-items-center d-flex">
-
-            Map Here
-
-          </div>
+          <MapContainer className="map-data" center={[55.953251, -3.188267]} zoom={13} scrollWheelZoom={true}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> IntelliDigest - iTracker'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[55.953251, -3.188267]}>
+              <Popup>
+                Pop up with some information about the marker. <br /> To be filled out.
+              </Popup>
+            </Marker>
+          </MapContainer>
         </Col>
         <Col className="" xs={12} lg={1}></Col>
         
