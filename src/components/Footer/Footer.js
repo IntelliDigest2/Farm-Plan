504d3b5f06ba84e2f99119f0aed568b7dcec3810
iTrackerPlatform <80331423+iTrackerPlatform@users.noreply.github.com>
2021-04-01 14:33:13 +0100

    social media links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,15 @@ function Footer({title,message}) {
     <div  className="footer-div">
       <footer>
       <i className="fa fa-copyright copy"> Copyright - IntelliDigest 2021</i>
+      <a href="https://www.linkedin.com/company/intellidigest-limited/?viewAsMember=true">
+        <i className="fa fa-linkedin social-media"></i>
+      </a>
+      <a href="https://www.facebook.com/IntelliDigest.ltd">
+        <i className="fa fa-facebook social-media"></i>
+      </a>
+      <a href="https://twitter.com/IntelliDigest">
+      <i className="fa fa-twitter social-media"></i>
+      </a>
       </footer>
     </div>
   );
