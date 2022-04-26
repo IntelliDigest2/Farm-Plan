cf293af3abdb59d555ebfbaf157072c240210cff
wftID3 <psyah10@exmail.nottingham.ac.uk>
2022-04-26 16:32:24 +0100

    Added units to nutrients

--- src/components/Pages/Account/Farm/Marketplace/Nutrients.jsx
+++ src/components/Pages/Account/Farm/Marketplace/Nutrients.jsx
@@ -13,9 +13,9 @@ export const Nutrients = (props) => {
     <List>
       {props.data.phosphorus && (
         <>
-          <h3>Phosphorus: {props.data.phosphorus}</h3>
-          <h3>Potassium: {props.data.potassium}</h3>
-          <h3>Potassium: {props.data.potassium}</h3>
+          <h3>Phosphorus: {props.data.phosphorus} Kg/Ha</h3>
+          <h3>Potassium: {props.data.potassium} Kg/Ha</h3>
+          <h3>Potassium: {props.data.potassium} Kg/Ha</h3>
         </>
       )}
       {!props.data.phosphorus && <p>No data available</p>}
