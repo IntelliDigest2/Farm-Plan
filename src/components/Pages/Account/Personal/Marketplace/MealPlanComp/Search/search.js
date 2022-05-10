3023506b1208800b143f241d723f9829a4a119da
IntelliDigest1 <M.Driscoll@IntelliDigest.com>
2022-05-10 17:18:02 +0100

    search via food origin

--- src/components/Pages/Account/Personal/Marketplace/MealPlanComp/Search/search.js
+++ src/components/Pages/Account/Personal/Marketplace/MealPlanComp/Search/search.js
@@ -1,11 +1,16 @@
 //api docs here https://developer.edamam.com/edamam-docs-recipe-api#/
-export const recipeSearch = async (query, mealType, setRecipes) => {
+export const recipeSearch = async (
+  query,
+  mealType,
+  cuisineType,
+  setRecipes
+) => {
   const app_id = "5532003c";
   const app_key = "511d39184173c54ebc5d02a5063a7b87";
   const resp = await fetch(
-    `https://api.edamam.com/api/recipes/v2?app_id=${app_id}&app_key=${app_key}&type=public&q=${query}${mealType}`
+    `https://api.edamam.com/api/recipes/v2?app_id=${app_id}&app_key=${app_key}&type=public&q=${query}${mealType}${cuisineType}`
   );
   const data = await resp.json();
   setRecipes(data.hits);
-  //   console.log("This is your data", data);
+  // console.log("This is your data", data);
 };
