d2ca2c75f310a3eacb7b98dcfa30e9ec02753a30
wftID3 <WFTracker1@gmail.com>
2022-07-19 17:15:49 +0300

    Shopping list now shows warning on items in inv
    - Shopping list items now have a warning icon to show that item is in inventory
    - ShopItems.js now fetches data for both shopping list and inventory to determine if warning icon should be shown
    - Added new inventory types in mealPlanReducer (get, get error, create, create error, delete, delete error)
    - inventoryData now dispatches new types on actions

--- src/store/reducers/mealPlanReducer.js
+++ src/store/reducers/mealPlanReducer.js
@@ -2,6 +2,7 @@ const initState = {
   authError: null,
   meals: [],
   shoppingList: [],
+  inventory: [],
 };
 
 const mealPlanReducer = (state = initState, action) => {
@@ -68,6 +69,43 @@ const mealPlanReducer = (state = initState, action) => {
         ...state,
         authError: "Edit meal failed",
       };
+    case "GET_INVENTORY":
+      console.log("get inventory success", action.payload);
+      return {
+        ...state,
+        inventory: action.payload,
+        authError: null,
+      };
+    case "GET_INVENTORY_ERROR":
+      console.log("error, couldn't fetch inventory", action.err);
+      return {
+        ...state,
+        authError: "Get inventory failed",
+      };
+    case "CREATE_INVENTORY_ITEM":
+      console.log("inventory item created", action.mealPlan);
+      return {
+        ...state,
+        authError: null,
+      };
+    case "CREATE_INVENTORY_ITEM_ERROR":
+      console.log("error, inventory item not created", action.err);
+      return {
+        ...state,
+        authError: "Create inventory item failed",
+      };
+      case "DELETE_INVENTORY_ITEM":
+        console.log("successfully deleted", action.mealPlan);
+        return {
+          ...state,
+          authError: null,
+        };
+      case "DELETE_INVENTORY_ITEM_ERROR":
+        console.log("error, couldn't delete inventory item", action.err);
+        return {
+          ...state,
+          authError: "Edit inventory item failed",
+        };
     default:
       return state;
   }
