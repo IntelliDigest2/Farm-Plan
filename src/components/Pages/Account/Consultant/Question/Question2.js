e542e7f94bf56305c50e4cda54f6bfa036025aa4
wftID3 <103274056+wftID3@users.noreply.github.com>
2022-08-03 01:30:35 +0200

    worked on the form part by :
    - adding the hide/show password icon
    - adding fixed prices to some of the consultant services
    -fixing the way responses are being sent to the database

--- src/components/Pages/Account/Consultant/Question/Question2.js
+++ src/components/Pages/Account/Consultant/Question/Question2.js
@@ -3,35 +3,32 @@ import "./Question.css"
 import { useHistory } from 'react-router-dom';
 import Form from "react-bootstrap/Form";
 import {connect} from "react-redux"
-
+import {BsEyeSlash , BsEye} from "react-icons/bs";
 import { createExample } from "../../../../../store/actions/consultingActions"
+import { FormControl } from 'react-bootstrap';
 
 
 
 const Question2 = (props) => {
 const[completed,setCompleted] =useState(false)
+const[show,setShow] =useState("password")
 const [user, setUser] =useState({
   fullName: "",
   email :"",
   urlLink :"",
   experience :"",
   expertise :"",
-  phoneCall:"",
-  onlineMeeting:"",
-  writtenFeedback:"",
-  inPersonVisit :"",
+  password : "",
   consultantVisit:"",
   summary:""
 })
 const history = useHistory();
 
 
-
+ 
 // for the form
 function handleSubmit(){
- var data ={
-    user : user,
-  }
+ var data ={...user}
   props.createExample(data);
   setCompleted(true)
   }
@@ -54,7 +51,7 @@ function handleSubmit(){
     <div className='question-contanier'>
         <section style={{height:"auto"}} className="question-main">
             <header className='logo-header'>
-        <img src='Green.png' alt='logo' className='logo1-btn'/>
+        <img src='/green.png' alt='logo' className='logo1-btn'/>
         <p>Become a Consultant with us.</p>
         </header>
 {/* bootstrap form */}
@@ -79,123 +76,61 @@ function handleSubmit(){
            </Form.Group>
 
 
+           <Form.Group controlId="formBasicPassword" className='form-group'>
+            <Form.Label className='form-label'>3. Password<span style={{color:"red"}}>*</span></Form.Label>
+            <Form.Control type={show} className='form-control'  onChange = {(e)=> setUser({...user, password: e.target.value})} required>
+            </Form.Control>
+            {/*for the show/hide password  */}
+          {show == "password" ? <BsEyeSlash className='eye-style' onClick={()=>setShow("text")}/>
+          :<BsEye className='eye-style' onClick={()=>setShow("password")}/>
+          }
+      </Form.Group>
+
+
            <Form.Group controlId="formBasicUrl" className='form-group'>
-           <Form.Label className='form-label'>3. Website Url(social media link)<span style={{color:"red"}}>*</span></Form.Label>
+           <Form.Label className='form-label'>4. Website Url(social media link)<span style={{color:"red"}}>*</span></Form.Label>
            <Form.Control type="text"  className='form-control'  onChange ={(e)=> setUser({...user, urlLink: e.target.value})} required></Form.Control>
       </Form.Group>
 
            <Form.Group controlId="formBasicText" className='form-group'>
-           <Form.Label className='form-label1'> 4. How Long have you been in business?<span style={{color:"red"}}>*</span></Form.Label>
+           <Form.Label className='form-label1'> 5. How Long have you been in business?<span style={{color:"red"}}>*</span></Form.Label>
           <Form.Control type="text"  className='form-control'  onChange ={(e)=> setUser({...user, experience: e.target.value})} required></Form.Control>
          </Form.Group>
 
          <Form.Group className='form-group'>
-<Form.Label className='form-label'>5. Field of Expertise<span style={{color:"red"}}>*</span></Form.Label>
+<Form.Label className='form-label'>6. Field of Expertise<span style={{color:"red"}}>*</span></Form.Label>
 <Form.Control as ="select"  className='form-control'  onChange ={(e)=> setUser({...user, expertise: e.target.value})}required>
 <option>Select</option>
 <option>Dietician</option>
 <option>Nutrition</option>
 <option>Food and Beverage</option>
+<option>Food Safety</option>
+<option>Sustainable Food Packaging</option>
 <option>Aquaculture</option>
 <option>Horticulture</option>
 <option>Agro-Feed</option>
+<option>Account and Legal</option>
+<option>Supply Chain</option>
 </Form.Control>
 </Form.Group>
 
 
-<Form.Group className='form-group'>
+<Form.Group className='form-group  different'>
 <Form.Label>6. Service Charge (hourly rate)<span style={{color:"red"}}>*</span></Form.Label>
-<Form.Control as ="select"  className='form-control'  onChange ={(e)=> setUser({...user, phoneCall: e.target.value})} required>
-<option>Phone Call</option>
-<option>$20</option>
-<option>$25</option>
-<option>$30</option>
-<option>$35</option>
-<option>$50</option>
-<option>$65</option>
-<option>$70</option>
-<option>$75</option>
-<option>$80</option>
-<option>$90</option>
-<option>$95</option>
-<option>$100</option>
-<option>$120</option>
-<option>$125</option>
-<option>other</option>
-</Form.Control>
+<FormControl className='form-control  different' type='text' placeholder='Phone Call' disabled></FormControl><span className='fixed-service-money '>$100</span>
+<FormControl className='form-control  different' type='text' placeholder='Written Feedback' disabled></FormControl><span className='fixed-service-money'>$60</span>
+<FormControl className='form-control  different' type='text' placeholder='Online Meeting' disabled></FormControl><span className='fixed-service-money'>$100</span>
+<FormControl className='form-control  different' type='text' placeholder='In-person visit to Consultant' disabled></FormControl><span className='fixed-service-money'>$100</span>
+<FormControl type="text" placeholder="Consultant Visit to Client hourly charge"  className='form-control'  onChange ={(e)=> setUser({...user, consultantVisit: e.target.value})}required ></FormControl>
 
-
-
-<Form.Control as ="select"  className='form-control'  onChange ={(e)=> setUser({...user, onlineMeeting: e.target.value})} required>
-<option>Online Meeting</option>
-<option>$20</option>
-<option>$25</option>
-<option>$30</option>
-<option>$35</option>
-<option>$50</option>
-<option>$65</option>
-<option>$70</option>
-<option>$75</option>
-<option>$80</option>
-<option>$90</option>
-<option>$95</option>
-<option>$100</option>
-<option>$120</option>
-<option>$125</option>
-<option>other</option>
-</Form.Control>
-
-<Form.Control as ="select"  className='form-control' onChange ={(e)=> setUser({...user, writtenFeedback: e.target.value})} required>
-<option>Written Feedback</option>
-<option>$20</option>
-<option>$25</option>
-<option>$30</option>
-<option>$35</option>
-<option>$50</option>
-<option>$65</option>
-<option>$70</option>
-<option>$75</option>
-<option>$80</option>
-<option>$90</option>
-<option>$95</option>
-<option>$100</option>
-<option>$120</option>
-<option>$125</option>
-<option>other</option>
-</Form.Control>
-
-<Form.Control as ="select"  className='form-control'  onChange ={(e)=> setUser({...user, inPersonVisit: e.target.value})} required>
-<option>In-person visit to Consultant</option>
-<option>$20</option>
-<option>$25</option>
-<option>$30</option>
-<option>$35</option>
-<option>$50</option>
-<option>$65</option>
-<option>$70</option>
-<option>$75</option>
-<option>$80</option>
-<option>$90</option>
-<option>$95</option>
-<option>$100</option>
-<option>$120</option>
-<option>$125</option>
-<option>other</option>
-</Form.Control>
-
-<Form.Control type="text" placeholder="Consultant Visit to Client"  className='form-control'  onChange ={(e)=> setUser({...user, consultantVisit: e.target.value})}required ></Form.Control>
 </Form.Group>
-
-
-
-        
-              <Form.Group controlId="formBasicName" className='form-group'>
+<Form.Group controlId="formBasicSummary" className='form-group'>
                 <Form.Label className='form-label'>8. Brief Summary of your Expertise/Key areas in the Food System.<span style={{color:"red"}}>*</span></Form.Label>
              <Form.Control as="textarea" rows ={4} type="text" onChange ={(e)=> setUser({...user, summary: e.target.value})}   required></Form.Control>
               </Form.Group>
 
 
-              <Form.Group controlId="formBasicName" className='form-group'>
+              <Form.Group controlId="formBasicOptional" className='form-group'>
                 <Form.Label  className='form-label'>9. Any other thing you would like to share with us ?(optional)</Form.Label>
                 <Form.Control as="textarea" rows ={4} type="text"  ></Form.Control>
               </Form.Group>
@@ -223,3 +158,8 @@ export default connect(null, mapDispatchToProps)(Question2);
  
 
 
+
+
+
+        
+       
