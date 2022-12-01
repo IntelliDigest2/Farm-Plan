42afa71a2d24fbfa89708fb9d9c7132116e7a627
codedfinger <alexyikeh@gmail.com>
2022-12-01 11:38:05 +0100

    add: adjusted css and ade text edit

--- src/components/Pages/Account/Personal/Marketplace/MealPlanComp/CalendarPlan.js
+++ src/components/Pages/Account/Personal/Marketplace/MealPlanComp/CalendarPlan.js
@@ -51,11 +51,13 @@ export const CalendarPlan = ({ value, onChange }) => {
           ))}
         </div>
       </div>
+      <div className="row">
+        <div className="col-8" style={{textAlign: "left"}}>Add more meals to your calendar or edit added meals 🙂</div>
+        <div className="col-4" style={{textAlign: "right"}}><AddPlanModal value={value} show={show} setShow={setShow} /></div>
+      </div>
       <div className="plan-box">
         <div className="header">{chosenDay()}</div>
-
-        <MyPlans value={value} show={show} />
-        <AddPlanModal value={value} show={show} setShow={setShow} />
+        <div><MyPlans value={value} show={show} /></div>
       </div>
     </>
   );
