1af0a22d93627d08c442e72857b0632656f21fab
unknown <SusChemTrade@gmail.com>
2023-02-23 21:45:56 +0100

    installed the stripe package

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,6 @@ const functions = require("firebase-functions");
 //   response.send("Hello from Firebase!");
 // });
 
-const functions = require("firebase-functions");
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const express = require("express");
 const itrackerPaymentFunction = express();
 const admin = require("firebase-admin");
