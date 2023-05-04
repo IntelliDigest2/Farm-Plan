9ad7feec57e2f73ce6a3379d412a33c55290c2f8
WFT-Aliu <A.Badamasi@IntelliDigest.com>
2023-05-04 22:51:57 +0100

    added the username of the chats

---
+++ .history/src/components/Pages/Account/Consultant/ConsultantAccount_20230502191954.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+
+import ImageProfile from "./ConsultantSettings/ImageProfile";
+import ConsultantSettings from "./ConsultantSettings/ConsultantSettings";
+import ConsultantLogin from "./ConsultantAuth/ConsultantLogin";
+import classes from "./ConsultantAccount.module.css";
+import SessionsIcon from "./ConsutltantIcons/sessionsIcon";
+import CalendarIcon from "./ConsutltantIcons/calendarIcon";
+import RecordsIcon from "./ConsutltantIcons/recordsIcon";
+import TabLink from "./TabLink";
+import SettingsIcon from "./ConsutltantIcons/settingsIcon";
+import { Link, useRouteMatch, Redirect } from "react-router-dom";
+import { fetchConsultantData } from "../../../../store/actions/consultantActions/consultantActions";
+import { IconButton } from "../../../SubComponents/Button";
+
+const ConsultantAccountPage = (props) => {
+	const { url } = useRouteMatch();
+	const [current, setCurrent] = useState(
+		"https://thumbs.dreamstime.com/b/default-avatar-profile-trendy-style-social-media-user-icon-187599373.jpg"
+	);
+
+	const { isLoggedIn, consultantData, profile, auth } = props;
+
+	const [isAuthorized, setIsAuthorized] = useState(false);
+	const [userInfo, setUserInfo] = useState("");
+
+	const [show, setShow] = useState(false);
+	const [setting, setSetting] = useState(false);
+
+	useEffect(() => {
+		props.getConsultantData(auth.uid);
+	}, []);
+
+	console.log(profile, `this is the profile`);
+
+	useEffect(() => {
+		console.log(consultantData);
+		if (consultantData) {
+			setUserInfo(consultantData);
+		}
+	}, [consultantData]);
+
+	console.log(profile, `first`);
+	let consultantContent =
+		profile.isLoaded === false ? (
+			"loading..."
+		) : !auth.uid ? (
+			<Redirect to="/consultant/login" />
+		) : profile?.consultant === "active" ? (
+			// props.consultantData ? (
+			<>
+				<main className={classes.consultant_main}>
+					<header className={classes.consultant_header}>
+						<div>Intelli Logo</div>
+						<div className={classes.consultant_profile}>
+							<div className={classes.profile_Img}>
+								<img
+									alt="default for new accounts created"
+									className={classes.defaultImg}
+									src={current}
+								/>
+							</div>
+
+							<div className={classes.profile_info}>
+								Welcome <span>{userInfo.fullName}</span>
+							</div>
+							<div className={classes.consultant_settings}>
+								<Link to={`${url}/settings`}>
+									<SettingsIcon />
+								</Link>
+								{/* <AiOutlineSetting className={classes.settings_icon} /> */}
+							</div>
+						</div>
+					</header>
+
+					<section className={classes.consultant_nav}>
+						<TabLink
+							icon={<SessionsIcon />}
+							link={"/consultant/sessions"}
+							text="Sessions"
+							backgroundColor="#afba15"
+						/>
+						<IconButton
+							title="Check consultation requests"
+							icon="requests"
+							label="Requests"
+							color="turquoise"
+							goTo="/consultant/requests"
+						/>
+						<IconButton
+							title="Check consultation requests"
+							icon="sessions"
+							label="Sessions"
+							color="turquoise"
+							goTo="/consultant/Sessions"
+						/>
+						<IconButton
+							title="Check consultation requests"
+							icon="calendar"
+							label="Records"
+							color="turquoise"
+							goTo="/consultant/records"
+						/>
+
+						<TabLink
+							icon={<CalendarIcon />}
+							link={"/consultant/requests"}
+							text="Requests"
+							backgroundColor="#0c0847"
+						/>
+						<TabLink
+							icon={<RecordsIcon />}
+							link={"/consultant/records"}
+							text="Customer Records"
+							backgroundColor="#afba15"
+						/>
+					</section>
+					{show && <ImageProfile setShow={setShow} />}
+					{setting && <ConsultantSettings setting={setSetting} />}
+				</main>
+			</>
+		) : (
+			// )
+			<Redirect to="/consultant/register" />
+		);
+
+	return <>{consultantContent}</>;
+};
+
+const mapStateToProps = (state) => ({
+	consultantData: state.consultantState.consultantData,
+	isLoggedIn: state.consultantAuth.consultantLoggedIn,
+	profile: state.firebase.profile,
+	auth: state.firebase.auth,
+});
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		getConsultantData: (userId) => dispatch(fetchConsultantData(userId)),
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(ConsultantAccountPage);
+
+// export default SecondPage;
---
+++ .history/src/components/Pages/Account/Consultant/ConsultantRequests/consultantRequest_20230503081253.js
@@ -0,0 +1,111 @@
+import classes from "./consultantRequest.module.css";
+import { format, parseISO, isValid } from "date-fns";
+
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Button } from "react-bootstrap";
+import {
+	acceptBookingRequest,
+	cancelBookingRequest,
+} from "../../../../../store/actions/consultantActions/consultantActions";
+
+export const ConsultantRequest = (props) => {
+	const [cancelLoading, setCancelLoading] = useState(false);
+	const [acceptLoading, setAcceptLoading] = useState(false);
+	const {
+		event,
+		acceptBooking,
+		cancelBooking,
+
+		cancelLoad,
+		confirmLoad,
+		consultantData,
+		showDialog,
+		auth,
+	} = props;
+
+	let date = format(parseISO(event.start), "yyyy-mm-dd");
+	let startTime = format(parseISO(event.start), "hh:mm a");
+	let endTime = format(parseISO(event.end), "hh:mm a");
+
+	let prices = consultantData.services.reduce((o, u) => {
+		console.log(u);
+		o[u.service] = u.price;
+		return o;
+	}, {});
+
+	let serviceCost = prices[event.eventType];
+	console.log(serviceCost, "this is the service cost");
+	const acceptRequest = () => {
+		acceptBooking(event, auth.uid, auth.fullName, serviceCost);
+		// showDialog();
+		setAcceptLoading(true);
+	};
+	const rejectRequest = () => {
+		cancelBooking(auth.uid, event);
+		//showDialog
+		setCancelLoading(true);
+	};
+
+	useEffect(() => {
+		if (!confirmLoad) {
+			setAcceptLoading(false);
+		}
+	}, [cancelLoad, confirmLoad]);
+
+	return (
+		<div className={classes.cont}>
+			<div>request type: {event.eventType}</div>
+			<div>request date: {date}</div>
+			<div>start time: {startTime}</div>
+			<div>end time: {endTime}</div>
+
+			<Button
+				disabled={
+					event.status.requestAccepted || event.status.requestId === null
+				}
+				onClick={(e) => rejectRequest(e)}
+				variant="danger"
+			>
+				{event.status.requestAccepted
+					? "Canceled"
+					: cancelLoading
+					? "canceling..."
+					: "cancel Request"}
+			</Button>
+			<Button
+				disabled={
+					event.status.requestAccepted || event.status.requestId === null
+				}
+				onClick={(e) => acceptRequest(e)}
+			>
+				{event.status.requestAccepted
+					? "Accepted"
+					: acceptLoading
+					? "accepting..."
+					: "accept Request"}
+			</Button>
+		</div>
+	);
+};
+
+const mapStateToProps = (state) => ({
+	consultantData: state.consultantState.consultantData,
+	cancelLoad: state.consultantState.cancelBookingLoad,
+	confirmLoad: state.consultantState.acceptBookingLoad,
+	auth: state.firebase.auth,
+});
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		acceptBooking: (event, consultantId, consultantName, serviceCost) =>
+			dispatch(
+				acceptBookingRequest(event, consultantId, consultantName, serviceCost)
+			),
+
+		cancelBooking: (event, consultantId) =>
+			dispatch(cancelBookingRequest(event, consultantId)),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConsultantRequest);
