48e5bda8a7f0169b527fdc7e1511070c45dc88ed
codedfingers <alexyikeh@gmail.com>
2023-07-12 19:01:02 +0100

    hotfix: forgot password

--- src/components/Pages/ForgotPassword.js
+++ src/components/Pages/ForgotPassword.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
-import "../../App.css";
-import "./Pages.css";
-import styled from "styled-components";
 import { Row, Col, Form, Button, Card } from "react-bootstrap";
 import { connect } from "react-redux";
 import { resetPassword } from "../../store/actions/authActions";
+import "./ForgotPassword.css";
+import { submitNotification } from "../lib/Notifications";
 
 class ForgotPassword extends Component {
 	state = {
@@ -21,6 +20,8 @@ class ForgotPassword extends Component {
 	handleSubmit = (e) => {
 		e.preventDefault();
 		this.props.resetPassword(this.state);
+		submitNotification("Success", "Please check your mail for the reset link");
+
 	};
 
 	render() {
@@ -28,26 +29,22 @@ class ForgotPassword extends Component {
 		if (auth.uid) return <Redirect to="/account" />;
 
 		return (
-			<React.Fragment>
-				<Row className="mr-0 ml-0 mt-0 pt-0 mt-lg-5 pt-lg-5 justify-content-center align-items-center d-flex frg-pass">
-					<Col className="mt-0 pt-0 mb-0 pb-0 mt-lg-2 pt-lg-2" xs={12}></Col>
-					<Col className="mt-5 pt-5" xs={12}></Col>
-					<Col className="" xs={12} lg={4}></Col>
-					<Col
-						className=" justify-content-center align-items-center d-block mt-5 pt-5 mt-lg-0 pt-lg-0"
-						xs={12}
-						lg={4}
-					>
-						<CardStyle>
+			<div className="forgot-password-container">
+				<Row className="m-0 p-0 justify-content-center align-items-center d-flex frg-pass">
+					<Col xs={12}></Col>
+					<Col xs={12}></Col>
+					<Col xs={12} lg={4}></Col>
+					<Col xs={12} lg={4} className="justify-content-center align-items-center d-block mt-5 pt-5 mt-lg-0 pt-lg-0">
+						<div className="card">
 							<Card>
 								<Card.Body>
 									<Card.Text>
-										<FormStyle>
+										<div>
 											<h1 className="text-center">Reset Password</h1>
 											<Form onSubmit={this.handleSubmit}>
 												<Form.Group>
-													<Form.Label>Email</Form.Label>
 													<Form.Control
+														className="signup-input placeholder-input"
 														type="email"
 														id="email"
 														placeholder="Email"
@@ -55,9 +52,9 @@ class ForgotPassword extends Component {
 														onChange={this.handleChange}
 													/>
 												</Form.Group>
-												<Form.Group controlId="formActions">
-													<Button variant="dark" type="submit">
-														Reset Password
+												<Form.Group controlId="formActions" className="form-actions">
+													<Button className="signup-confirm" type="submit">
+														Reset
 													</Button>
 												</Form.Group>
 											</Form>
@@ -76,59 +73,20 @@ class ForgotPassword extends Component {
 											<div className="auth-error">
 												{authError ? <p> {authError}</p> : null}
 											</div>
-										</FormStyle>
+										</div>
 									</Card.Text>
 								</Card.Body>
 							</Card>
-						</CardStyle>
+						</div>
 					</Col>
-					<Col className="mt-5 pt-5" xs={12} lg={4}></Col>
-					<Col className="mt-5 pt-5" xs={12}></Col>
-					<Col className="mt-5 pt-5" xs={12}></Col>
+					<Col xs={12} lg={4}></Col>
+					<Col xs={12}></Col>
+					<Col xs={12}></Col>
 				</Row>
-			</React.Fragment>
+			</div>
 		);
 	}
 }
-
-const FormStyle = styled.div`
-	form {
-		margin: auto;
-		padding: 10px;
-		width: 100%;
-	}
-
-	input {
-		border: 1px solid #62680a;
-	}
-
-	.btn-dark {
-		background-color: #071850;
-		color: whitesmoke;
-		border: 1px solid #03091d;
-		float: right;
-
-		&:hover {
-			background-color: #030d2b;
-			border: 1px solid #03091d;
-		}
-
-		&:active {
-			background-color: #030d2b;
-			border: 1px solid #03091d;
-		}
-	}
-`;
-
-const CardStyle = styled.div`
-	.card {
-		color: rgb(59, 59, 59);
-		background-color: rgb(238, 238, 238);
-		border: none;
-		border-radius: 5px;
-		padding: 10px 0 10px 0;
-	}
-`;
 
 const mapStateToProps = (state) => {
 	return {
@@ -136,6 +94,7 @@ const mapStateToProps = (state) => {
 		auth: state.firebase.auth,
 	};
 };
+
 const mapDispatchToProps = (dispatch) => {
 	return {
 		resetPassword: (creds) => dispatch(resetPassword(creds)),
