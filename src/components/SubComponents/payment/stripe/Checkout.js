a640c7724921a92ecef7503022a20a7221215095
codedfingers <alexyikeh@gmail.com>
2023-09-11 12:52:32 +0100

    fix: balance update on deposit

--- src/components/SubComponents/payment/stripe/Checkout.js
+++ src/components/SubComponents/payment/stripe/Checkout.js
@@ -50,9 +50,9 @@ export default function CheckoutForm( props ) {
     });
   }, [stripe]);
 
-  const handleSuccess = async => {
+  const handleSuccess = async () => {
     try {
-        const response = fetch(`${baseUrlProd}/v1/transaction/deposit`, {
+        const response = await fetch(`${baseUrlProd}/v1/transaction/deposit`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
